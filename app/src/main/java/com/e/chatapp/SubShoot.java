f2017fe57b444a2d7f09802470e97262c6ef804b
SimingZheng <SimingZheng1999@gmail.com>
2020-04-19 22:04:40 +0100

    add notes about validation 2.4

---
+++ app/src/main/java/com/e/chatapp/SubShoot.java
@@ -0,0 +1,14 @@
+package com.e.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubShoot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_shoot);
+    }
+}
--- app/src/main/java/com/e/chatapp/ui/channel/ChannelFragment.java
+++ app/src/main/java/com/e/chatapp/ui/channel/ChannelFragment.java
@@ -139,7 +139,6 @@ public class ChannelFragment extends Fragment{
             }
         });
 
-
         return root;
     }
 
